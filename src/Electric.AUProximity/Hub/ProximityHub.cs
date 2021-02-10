8dba021d7905ba301f867986694e06e499708e35
miniduikboot <5243971+miniduikboot@users.noreply.github.com>
2021-02-10 21:03:29 +0100

    Add HostChange event to proximity plugin
    When the game host changes or when a connection is first established it
    will send the name of the host over.
    
    Bonus feature: also send over the map name when a connection is started.
    The official backend already had that and now the Impostor backend has too.

--- src/Electric.AUProximity/Hub/ProximityHub.cs
+++ src/Electric.AUProximity/Hub/ProximityHub.cs
@@ -1,13 +1,28 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
+using Impostor.Api.Games;
+using Impostor.Api.Games.Managers;
 
 namespace Electric.AUProximity.Hub
 {
     public class ProximityHub : Hub<IProximityHub>
     {
+        private readonly IGameManager _gameManager;
+
+        public ProximityHub(IGameManager gameManager)
+        {
+            _gameManager = gameManager;
+        }
+
         public async Task TrackGame(string gameCode)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, gameCode);
+            IGame? game = _gameManager.Find(gameCode);
+            if (game != null)
+            {
+                await this.Clients.Group(gameCode).MapChange(game.Options.Map);
+                await this.Clients.Group(gameCode).HostChange(game.Host.Client.Name);
+            }
         }
     }
 }
